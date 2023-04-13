6440ab6a6654c407a214ce632d3588ed70f3e0ff
Brian Jones <brian.jones.cs@gmail.com>
2023-04-13 12:48:51 -0700

    App text now updates with navigation

--- pages/navbar.tsx
+++ pages/navbar.tsx
@@ -1,30 +1,22 @@
-// import Link from 'next/link'
+import {useRouter} from 'next/router';
 import JBMenu from './dropdown';
-// import { useRouter } from 'next/router'
-// import styles from '@/styles/NavBar.module.css'
 
+const pageNames = {
+  'partscanner' : 'Part Scanner',
+  'blog' : 'Blog',
+  'about' : 'About Me'
+}
 export default function NavBar() {
-  // const router = useRouter();
+  const router = useRouter()
+  let currUrl: string = router.pathname.slice(1);
+  currUrl = pageNames.hasOwnProperty(currUrl) ? pageNames[currUrl as keyof typeof pageNames] : "";
 
     return (
       <>
       <div className='flex bg-black'> 
         <JBMenu />
-        <div className='flex-1 text-white text-center'>NAME OF CURRENT APP</div>
+        <div id='currapp' className='flex-1 text-white text-center'>{currUrl}</div>
       </div>
       </>
     )
   }
-
-  // <button className={styles.navbaritem} type="button" onClick={() => router.push('/')}>
-  // Home
-  // </button>
-  // <button className={styles.navbaritem} type="button" onClick={() => router.push('/about')}>
-  // About Us
-  // </button>
-  // <button className={styles.navbaritem} type="button" onClick={() => router.push('/partscanner')}>
-  // Part Scanner
-  // </button>
-  // <button className={styles.navbaritem} type="button" onClick={() => router.push('/blog')}>
-  // Blog
-  // </button>
