7336eef7e442055771f86b16639df6597e391e68
Brian Jones <brian.jones.cs@gmail.com>
2023-10-04 10:29:44 -0700

    Couple updated files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 // import styles from '@/styles/Home.module.css'
 import React, { FC, ReactNode, ReactPropTypes } from 'react';
-// import styles from '@/styles/Index.module.css'
+import Link from 'next/link'
+import styles from '@/styles/Index.module.css'
 import PartScanner from './partscanner'
 
+// TODO BJONES: Maybe try making the whole background the SVG swirly, and keep the text see through?
+// BJONES: sadly text shadow doesn't help because the text is actually a hole, to placing a shadow puts it on top of the background making it darker.
 // function makeGridItem(item: String) {
 //   return (
 //     <div className={styles.gridItem}>
@@ -29,13 +32,14 @@ export default function Home() {
   // let partScanNode: ReactNode = <PartScanner />;
   // let mainWindow = document.querySelector(".main");
   // let classes = 'main ' + styles.mainFlex;
+  let classes = "fancy flex flex-1 justify-center items-center bg-yellow-500 rounded-xl m-1 text-9xl font-sans font-bold";
   // let partScan = makeGridItem(<PartScanner />);
   return (
         <>
           <div className="flex flex-col justify-around h-screen m-4">
-            <Card className="flex flex-1 justify-center items-center bg-yellow-500 rounded-xl m-1" title="Part Scanner"/>
-            <Card className="flex flex-1 justify-center items-center bg-green-500 rounded-xl m-1" title="Blog"/>
-            <Card className="flex flex-1 justify-center items-center bg-blue-500 rounded-xl m-1" title="About Me"/>
+            <Card className="flex flex-1 justify-center items-center bg-yellow-500 rounded-xl m-4" title="Part Scanner" link="/partscanner"/>
+            <Card className="flex flex-1 justify-center items-center bg-green-500 rounded-xl m-4" title="Blog" link="/blog"/>
+            <Card className="flex flex-1 justify-center items-center bg-blue-500 rounded-xl m-4" title="About Me" link="/about"/>
           </div>
         </>
   )
@@ -44,14 +48,18 @@ export default function Home() {
 type CardInfo = {
   className: string,
   title: string,
+  link: string,
 }
 
 function Card(info: CardInfo) {
+  let classes = styles.cutout + " font-sans font-bold"
+  let linkclass = info.className + " " + styles.grade
   return (
     <>
-    <div className={info.className} > {info.title}
-      
+    <Link className={linkclass} href={info.link}>
+    <div className={classes} > {info.title}
     </div>
+    </Link>
     </>
   )
 }
